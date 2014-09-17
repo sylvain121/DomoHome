48c760f15b7e1f15d72ac2273624b2e7b6a24b6c
sylvain121 <sylvain121@gmail.com>
2014-09-17 13:06:32 +0200

    work in progress

--- serveur/serveur/database/index.js
+++ serveur/serveur/database/index.js
@@ -4,3 +4,10 @@ var Datastore = require('nedb');
 // db instances
 db.i2c = new Datastore({ filename: '../../db/i2c.db', autoload: true });
 db.network = new Datastore({ filename: '../../db/network.db', autoload: true });
+
+
+
+
+
+
+module.exports.db = db;
