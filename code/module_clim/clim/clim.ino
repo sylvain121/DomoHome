999971f48daef1191201223a331ac4daa392ceaa
sylvain MATHIEU <sylvain.mathieu@orange.com>
2015-11-06 16:54:57 +0100

    update clim file

--- code/module_clim/clim/clim.ino
+++ code/module_clim/clim/clim.ino
@@ -1,31 +1,57 @@
-/*
-
-Send to clim command
-
-*/
-
-#include <IRremote.h>
-
-#define HEADER_TIME_UP 2945
-#define HEADER_TIME_DOWN 1785
-#define BIT_TIME_DOWN 412
-#define BIT_TIME_UP 1147
-#define COMMAND_LENGTH 227
-#define COMMAND_FREQ_KHZ 38
+/* ATtiny85 as an Airotn InfraRed climatisation module
+ * Sylvain121
+ * AtTiny85 pin setup:
+ * ATtiny Pin 1 = (RESET) N/U                      ATtiny Pin 2 = (D3) N/U
+ * ATtiny Pin 3 = IR SEND	                       ATtiny Pin 4 = GND
+ * ATtiny Pin 5 = I2C SDA on DS1621  & GPIO        ATtiny Pin 6 = LM35 sensor input
+ * ATtiny Pin 7 = I2C SCK on DS1621  & GPIO        ATtiny Pin 8 = VCC (2.7-5.5V)
+ * NOTE! - It's very important to use pullups on the SDA & SCL lines!
+ * Current Rx & Tx buffers set at 32 bytes - see usiTwiSlave.h
+ * Credit and thanks to Don Blake for his usiTwiSlave code. 
+ * More on TinyWireS usage - see TinyWireS.h
+ */
 
 
+#include "TinyWireS.h"                  // wrapper class for I2C slave routines
+
+#define I2C_SLAVE_ADDR  0x26            // i2c slave address (38)
+#define AIRTON_IR_SEND_PIN      4              // ATtiny Pin 3
+#define LM35_SENSOR_PIN         1              // ATtiny Pin 6
 
 
-unsigned int on_hot_24c[] = {2944, 1784, 416, 1132, 392, 1160, 412, 412, 388, 436, 392, 500, 348, 1140, 408, 420, 404, 428, 412, 1136, 388, 1160, 388, 436, 392, 1156, 416, 408, 420, 408, 416, 1132, 416, 1148, 416, 408, 416, 1132, 416, 1116, 412, 432, 392, 432, 416, 1128, 396, 432, 420, 448, 388, 1136, 392, 432, 392, 432, 416, 408, 420, 432, 368, 432, 416, 408, 420, 420, 416, 408, 392, 432, 420, 404, 420, 416, 412, 416, 376, 464, 392, 404, 420, 432, 408, 432, 392, 408, 392, 1152, 424, 404, 420, 404, 420, 1136, 412, 408, 420, 420, 416, 1136, 416, 404, 420, 476, 348, 404, 420, 404, 424, 404, 420, 404, 420, 420, 420, 1128, 420, 1128, 396, 1160, 412, 408, 420, 404, 416, 436, 392, 412, 388, 456, 384, 1160, 416, 412, 412, 1124, 424, 1124, 428, 1124, 420, 1128, 424, 404, 416, 424, 416, 416, 412, 404, 420, 404, 396, 448, 376, 440, 384, 432, 420, 404, 420, 424, 412, 408, 392, 432, 392, 504, 324, 432, 416, 412, 416, 476, 324, 444, 380, 456, 408, 420, 404, 416, 384, 432, 416, 408, 392, 432, 396, 428, 396, 432, 416, 424, 416, 408, 416, 408, 420, 408, 388, 436, 416, 408, 420, 404, 416, 408, 392, 448, 420, 404, 416, 1132, 420, 1128, 420, 1128, 396, 1156, 392, 1156, 416, 1132, 392, 440, 416};
+#define MSG_GET_TEMP 0x8000
+#define MSG_SEND_IR	 0xFFFF
 
-IRsend irsend;
-
-void setup()
-{
-	Serial.begin(9600);
+void setup(){
+  pinMode(LED1_PIN,INPUT);             // for general DEBUG use
+  TinyWireS.begin(I2C_SLAVE_ADDR);      // init I2C Slave mode
 }
 
-void loop() {
-	     irsend.sendRaw(on_hot_24c, 227,38);
-       delay(3000);
+
+void loop(){
+  byte byteRcvd = 0;
+  if (TinyWireS.available()){           // got I2C input!
+    byteRcvd = TinyWireS.receive();     // get the byte from master
+	switch(byteRcvd) {
+    case MSG_SEND_IR:
+    case MSG_GET_TEMP:
+
+    
+
+	}    
+
+    TinyWireS.send(byteRcvd);           // send it back to master
+
+  }
 }
+
+int getTemperature() {
+
+	int val = analogRead(LM35_SENSOR_PIN);
+	float mv(val/1024.0)*5000;
+	float cel = mv/10;
+	return cel;	
+}
+
+
+
