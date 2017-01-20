3bce8cd6e27f4abc11f9fb4124c5fcc1934a132c
sylvain121 <sylvain121@gmail.com>
2017-01-20 13:05:20 +0000

    add things manager collection, create driver manager

--- serveur/serveur/mDns/discovery.js
+++ serveur/serveur/mDns/discovery.js
@@ -1,14 +1,19 @@
 var mdns = require('mdns');
 
-var moduleBrowser = mdns.createBrowser(mdns.tcp('IoTModule'));
+
+module.exports.init = function(newThingConnectionHandler, thingDisconnectionHandler) {
+
+  var moduleBrowser = mdns.createBrowser(mdns.tcp('IoTModule'));
 
 
-moduleBrowser.on('serviceUp', function(service){
-	console.log("service up : ", service);
-});
+  moduleBrowser.on('serviceUp', function(service) {
+    console.log("service up : ", service);
+  });
 
-moduleBrowser.on('ServiceDown', function(service){
-	console.log('service down : ',service);
-});
+  moduleBrowser.on('ServiceDown', function(service) {
+    console.log('service down : ', service);
+  });
 
-moduleBrowser.start();
+  moduleBrowser.start();
+}
+
---
+++ serveur/serveur/things/manager.js
@@ -0,0 +1,43 @@
+var request = require('request');
+var driverManager = require('../drivers/driversManager.js');
+var things = [];
+
+
+function newThingConnectionHandler(ipV4Address) {
+  getModuleInfo(ipV4Address)
+    .then(thing => {
+      return driverManager.getDriverbyName(things)
+    })
+    .then(thing => {
+      things.push(thing);
+    })
+    .catch(err => {
+      console.err(err);
+    });
+
+
+}
+
+
+function thingDisconnectionHandler(ipV4Address) {
+  things = things.filter(thing => {
+    return thing.ip != ipV4Address;
+  });
+}
+
+
+function getModuleInfo(ipV4Address) {
+  return new Promise((resolve, reject) => {
+    request("http://" + ipV4Address + "/getInfo",
+      (err, response, body) {
+        if(err) return reject(err);
+        return resolve(body);
+      });
+  });
+}
+
+module.exports = {
+  newThingConnectionHandler,
+  thingDisconnectionHandler
+}
+
