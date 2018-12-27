b503709fd557e3298aceadfa5ab5ddfae28347f7
sylvain121 <sylvain121@gmail.com>
2018-12-27 20:56:48 +0100

    fix route

--- server/domohome/routes/index.ts
+++ server/domohome/routes/index.ts
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
-import * as airton from '../src/AirtonService/AirtonDriver';
-
+const airton =require('../src/AirtonService/AirtonDriver');
 /* GET home page. */
-router.get('/', function(req: any, res: any, next:any) {
+router.get('/', function(req: any, res: any, nxt:any) {
   res.render('index', { title: 'Express' });
 });
 
 
 router.get('/heating/:room/off', function(req:any, res:any) {
 	airton.setOff();
+	res.status(200);
 });
 
 //FIXME should be post
@@ -17,6 +17,7 @@ router.get('/heating/:room/:temperature', function(req:any, res:any, next:any) {
 	const room = req.query.room; // currently ignoring room 
 	const temp = req.query.temperature;
 	airton.setOn24Hot();
+	res.status(200);
 });
 
 
